20c0b7c003f31329b645f3005bf934131c6481a9
Monica Hong <momo.star@gmail.com>
2023-11-11 21:10:15 -0800

    change ternary

--- 11.container-with-most-water.js
+++ 11.container-with-most-water.js
@@ -9,42 +9,37 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    let len = height.length;
-    // area is calculated by l x h
-    let rightPos = len - 1
-    let leftPos = 0
-    let maxArea = 0
-    
-    // loop from both ends of array
-    while(leftPos < rightPos) {
-        let leftWall = height[leftPos]
-        let rightWall = height[rightPos]
-        let distance = rightPos - leftPos;
-        let currentArea = 0
-        
-        // multiply the l x smaller height between the 2 walls (area)
-        leftWall > rightWall
-        ? currentArea = rightWall * distance
-        : currentArea = leftWall * distance
-        
-        console.log(currentArea)
-        // compare to maxArea and reassign if higher
-        if (currentArea > maxArea) {
-            maxArea = currentArea
-        }
-        // check if next walls are bigger
-        if (leftWall < rightWall) {
-            leftPos++
-        }
-        else {
-            rightPos--
-        }
+var maxArea = function (height) {
+  let len = height.length;
+  // area is calculated by l x h
+  let rightPos = len - 1;
+  let leftPos = 0;
+  let maxArea = 0;
+
+  // loop from both ends of array
+  // avoid negative distance
+  while (leftPos < rightPos) {
+    let leftWall = height[leftPos];
+    let rightWall = height[rightPos];
+    let distance = rightPos - leftPos;
+    let currentArea = 0;
+
+    // multiply the l x smaller height between the 2 walls (area)
+    leftWall > rightWall
+      ? (currentArea = rightWall * distance)
+      : (currentArea = leftWall * distance);
+
+    console.log(currentArea);
+    // compare to maxArea and reassign if higher
+    if (currentArea > maxArea) {
+      maxArea = currentArea;
     }
-    return maxArea
+    // check which of the walls are smaller
+    leftWall < rightWall ? leftPos++ : rightPos--;
+  }
+  return maxArea;
 };
 // @lc code=end
-
 
 // @after-stub-for-debug-begin
 module.exports = maxArea;
