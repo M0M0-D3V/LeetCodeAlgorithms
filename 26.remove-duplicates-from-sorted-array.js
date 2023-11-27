450d7f4b36c436505d6b1506118e128c39f037fd
Monica Hong <momo.star@gmail.com>
2023-11-26 19:19:09 -0800

    11/26/23

--- 26.remove-duplicates-from-sorted-array.js
+++ 26.remove-duplicates-from-sorted-array.js
@@ -9,8 +9,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    
+var removeDuplicates = function (nums) {
+  if (!nums.length) {
+    return 0;
+  }
+
+  let k = 1; // Initialize the count of unique elements to 1
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] != nums[i - 1]) {
+      nums[k] = nums[i]; // Overwrite the next unique element
+      k++;
+    }
+  }
+
+  return k;
 };
 // @lc code=end
 
+// @after-stub-for-debug-begin
+module.exports = removeDuplicates;
+// @after-stub-for-debug-end
