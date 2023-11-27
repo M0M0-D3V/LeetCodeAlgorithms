675b2c872d957f13a4b9d26c891363fa1c219f82
Monica Hong <momo.star@gmail.com>
2023-11-27 03:51:17 -0800

    11/27/23

---
+++ 35.search-insert-position.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=35 lang=javascript
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  // search using midpoint
+  // if nums contains target, return index found
+  // if no target, return to place
+};
+// @lc code=end
